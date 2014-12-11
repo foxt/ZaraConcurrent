acab888303144d94c91ad79a4a25124067916719
foxt <tyler.fox09@gmail.com>
2014-12-11 05:12:10 -0500

    Fixed the map

--- CelebSearch/CelebResults/views.py
+++ CelebSearch/CelebResults/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import loader, RequestContext
 from django.shortcuts import render_to_response
 from multiprocessing import Pool
+from geopy.geocoders import Nominatim
 
 from CelebResults.models import Clips_Adaptor, Mongo_Service
 
@@ -25,6 +26,7 @@ def results(request):
     if request.method == "GET":
         clips = Clips_Adaptor()
         mongodb = Mongo_Service()
+        geolocator = Nominatim()
         name = request.GET.__getitem__("artistname")
 
         ## If no input given, stay on search page
@@ -43,6 +45,7 @@ def results(request):
             convert = True
 
         coords = []
+        countries = {}
         for index, tweet in enumerate(tweets):
             ## if the tweets came directly from clips, convert the unicode 
             ## to a dictionary
@@ -54,10 +57,32 @@ def results(request):
             try:
                 curr_coords = tweet["coordinates"]
                 if curr_coords["type"] == 'Point':
-                    coords.append(curr_coords["coordinates"])
+                    curr_coords = "{0}, {1}".format(curr_coords["coordinates"][1],curr_coords["coordinates"][0])
+                    location = geolocator.reverse(curr_coords)
+                    try:
+                        country = location.raw['address']['country']
+                    except:
+                        country = None
+                        
+                    if country != None:
+                        if country == "United States of America":
+                            country = "United States"
+                        if country == "Deutschland":
+                            country = "Germany"
+                       
+                        if country in countries:
+                            countries[country] = countries[country] + 1
+                        else:
+                            countries[country] = 1
+                    
+                    coords.append(curr_coords)
             except:
                 coords.append([-200,-200])
-                
+        
+        country_list = []
+        for country in countries:
+            country_list.append([str(country), countries[country]])
+
         ## Score the tweets with sentiment analysis
         pool = Pool(processes=5)
         scores = pool.map(get_sentiment_concur, tweets)
@@ -67,7 +92,7 @@ def results(request):
             score = score.split(',')
             scores[index] = (score[0],score[1])
 
-        context = RequestContext(request, {'tweets':tweets, 'scores': scores, 'coordinates': coords})
+        context = RequestContext(request, {'tweets':tweets, 'scores': scores, 'country_list': country_list})
         return render_to_response('CelebResults/results.html', context)
         
 
